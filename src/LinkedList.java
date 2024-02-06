795f5bbb6d93d928c3f9baf2e31deb2278c61991
heeneth <heeneth123@gmail.com>
2024-02-06 10:14:54 +0530

    addind data at first position in LL

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,31 +1,31 @@
 //uc1 create a simple linked list
-
 // Node class represents a node in a linked list
-class Node {
-    int data; // Data of the node
-    Node next; // Reference to the next node in the list
+class Node<T> {
+    T data; // Data of the node
+    Node<T> next; // Reference to the next node in the list
 
     // Constructor to initialize a node with given data
-    public Node(int data) {
+    public Node(T data) {
         this.data = data;
         this.next = null; // Initially, the next node is set to null
     }
 }
 
 // MyLinkedList class manages the linked list operations
-class MyLinkedList {
-    Node head; // The starting point of the linked list
+class MyLinkedList<T> {
+    Node<T> head; // The starting point of the linked list
 
     // Method to add a new node with given data to the end of the linked list
-    public void add(int data) {
-        Node newNode = new Node(data); // Create a new node with the provided data
+    public void add(T data) {
+        Node<T> newNode = new Node<T>(data); // Create a new node with the provided data
 
         if (head == null) {
             // If the list is empty, set the new node as the head
             head = newNode;
         } else {
             // If the list is not empty, traverse to the end and add the new node
-            Node current = head;
+            Node<T> current = head;
+            // temp node current
             while (current.next != null) {
                 current = current.next; // Move to the next node
             }
@@ -33,11 +33,19 @@ class MyLinkedList {
         }
     }
 
+    // ---> adding at first
+    public void addFirst(T data) {
+        Node<T> addFirstPosNode = new Node<T>(data);
+        addFirstPosNode.next = head;
+        head = addFirstPosNode;
+
+    }
+
     // Method to display the contents of the linked list
     public void display() {
-        Node current = head; // Start from the head of the list
+        Node<T> current = head; // Start from the head of the list
         while (current != null) {
-            System.out.print(current.data + " "); // Print the data of the current node
+            System.out.print(current.data + " -> "); // Print the data of the current node
             current = current.next; // Move to the next node
         }
         System.out.println(); // Print a new line after displaying all nodes
@@ -47,12 +55,12 @@ class MyLinkedList {
 // ExampleLinkedList class demonstrates the usage of MyLinkedList
 public class LinkedList {
     public static void main(String[] args) {
-        MyLinkedList myLinkedList = new MyLinkedList(); // Create an instance of MyLinkedList
+        MyLinkedList<Integer> myLinkedList = new MyLinkedList<Integer>(); // Create an instance of MyLinkedList
 
         // Add nodes with data 12, 13, and 14 to the linked list
-        myLinkedList.add(12);
-        myLinkedList.add(13);
-        myLinkedList.add(14);
+        myLinkedList.addFirst(70);
+        myLinkedList.addFirst(30);
+        myLinkedList.addFirst(56);
 
         System.out.println("Linked List:");
         myLinkedList.display(); // Display the contents of the linked list
