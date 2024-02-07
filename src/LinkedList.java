4d1fb97be89e85bbb5885eb23308b5dba7525eb3
heeneth <heeneth123@gmail.com>
2024-02-07 09:58:05 +0530

    added ability to appending data

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -27,7 +27,7 @@ class MyLinkedList<T> {
             Node<T> current = head;
             // temp node current
             while (current.next != null) {
-                current = current.next; // Move to the next node
+                // Move to the next node
             }
             current.next = newNode; // Set the new node as the next node of the last node
         }
@@ -37,8 +37,30 @@ class MyLinkedList<T> {
     public void addFirst(T data) {
         Node<T> addFirstPosNode = new Node<T>(data);
         addFirstPosNode.next = head;
+        // making next part [] == null
         head = addFirstPosNode;
+        // putting address in head
 
+    }
+
+    // ---> appending data
+    public void appendingData(T data) {
+        Node<T> newNode = new Node<T>(data); // Create a new node with the provided data
+
+        if (head == null) {
+            // If the list is empty, set the new node as the head
+            head = newNode;
+        } else {
+            // If the list is not empty, traverse to the end and add the new node
+            Node<T> current = head;
+            // temp node current
+            while (current.next != null) {
+                // Move to the next node\
+                current = current.next;
+            }
+            current.next = newNode; // Set the new node as the next node of the last node
+
+        }
     }
 
     // Method to display the contents of the linked list
@@ -58,9 +80,9 @@ public class LinkedList {
         MyLinkedList<Integer> myLinkedList = new MyLinkedList<Integer>(); // Create an instance of MyLinkedList
 
         // Add nodes with data 12, 13, and 14 to the linked list
-        myLinkedList.addFirst(70);
-        myLinkedList.addFirst(30);
-        myLinkedList.addFirst(56);
+        myLinkedList.appendingData(56);
+        myLinkedList.appendingData(30);
+        myLinkedList.appendingData(70);
 
         System.out.println("Linked List:");
         myLinkedList.display(); // Display the contents of the linked list
